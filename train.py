a17bb3a256d3019e601f6017b5966d6673295ac2
matmons <mons.e.mathiesen@gmail.com>
2021-04-08 10:19:42 +0200

    train

--- train.py
+++ train.py
@@ -65,12 +65,13 @@ def train(model, train_dl, valid_dl, loss_fn, optimizer, acc_fn, params_path, ep
                 # stats - whatever is the phase
                 acc = acc_fn(outputs, y)
 
-                running_acc  += acc*dataloader.batch_size
-                running_loss += loss*dataloader.batch_size 
+                running_acc += acc * dataloader.batch_size
+                running_loss += loss * dataloader.batch_size
 
                 if step % 100 == 0:
                     # clear_output(wait=True)
-                    print('Current step: {}  Loss: {}  Acc: {}  AllocMem (Mb): {}'.format(step, loss, acc, torch.cuda.memory_allocated()/1024/1024))
+                    print('Current step: {}  Loss: {}  Acc: {}  AllocMem (Mb): {}'.format(step, loss, acc,
+                                                                                          torch.cuda.memory_allocated() / 1024 / 1024))
                     # print(torch.cuda.memory_summary())
 
             epoch_loss = running_loss / len(dataloader.dataset)
@@ -80,50 +81,54 @@ def train(model, train_dl, valid_dl, loss_fn, optimizer, acc_fn, params_path, ep
             print('-' * 10)
             print('{} Loss: {:.4f} Acc: {}'.format(phase, epoch_loss, epoch_acc))
             print('-' * 10)
-        
-            train_loss.append(epoch_loss) if phase=='train' else valid_loss.append(epoch_loss)
-        torch.save(model.state_dict(), params_path+f'{epoch}.pth')
-    torch.save(model.state_dict(), params_path+'final.pth')
+
+            train_loss.append(epoch_loss) if phase == 'train' else valid_loss.append(epoch_loss)
+        torch.save(model.state_dict(), params_path + f'{epoch}.pth')
+    torch.save(model.state_dict(), params_path + 'final.pth')
     time_elapsed = time.time() - start
-    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))    
-    
-    return train_loss, valid_loss    
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    return train_loss, valid_loss
+
 
 def acc_metric(predb, yb):
     return (predb.argmax(dim=1) == yb.cuda()).float().mean()
 
+
 def batch_to_img(xb, idx):
-    img = np.array(xb[idx,0:3])
-    return img.transpose((1,2,0))
+    img = np.array(xb[idx, 0:3])
+    return img.transpose((1, 2, 0))
+
 
 def predb_to_mask(predb, idx):
     p = torch.functional.F.softmax(predb[idx], 0)
     return p.argmax(0).cpu()
 
-def main ():
-    #enable if you want to see some plotting
+
+def main():
+    # enable if you want to see some plotting
     visual_debug = True
     params_path = 'models/model_epoch_'
 
-    #batch size
+    # batch size
     bs = 12
 
-    #epochs
+    # epochs
     epochs_val = 50
 
-    #learning rate
+    # learning rate
     learn_rate = 0.01
 
-    #sets the matplotlib display backend (most likely not needed)
-    #mp.use('TkAgg', force=True)
+    # sets the matplotlib display backend (most likely not needed)
+    # mp.use('TkAgg', force=True)
 
-    #load the training data
+    # load the training data
     base_path = Path('/work/datasets/medical_project/CAMUS_resized')
-    data = DatasetLoader(base_path/'train_gray', 
-                        base_path/'train_gt')
+    data = DatasetLoader(base_path / 'train_gray',
+                         base_path / 'train_gt')
     print(len(data))
 
-    #split the training dataset and initialize the data loaders
+    # split the training dataset and initialize the data loaders
     train_dataset, valid_dataset, _ = torch.utils.data.random_split(
         data,
         (300, 100, 50),
@@ -133,7 +138,7 @@ def main ():
     valid_data = DataLoader(valid_dataset, batch_size=bs, shuffle=True)
 
     if visual_debug:
-        fig, ax = plt.subplots(1,2)
+        fig, ax = plt.subplots(1, 2)
         ax[0].imshow(data.open_as_array(150))
         ax[1].imshow(data.open_mask(150))
         plt.show()
@@ -142,38 +147,39 @@ def main ():
     print(xb.shape, yb.shape)
 
     # build the Unet2D with one channel as input and 2 channels as output
-    unet = Unet2D(1,2)
+    unet = Unet2D(1, 2)
 
-    #loss function and optimizer
+    # loss function and optimizer
     loss_fn = nn.CrossEntropyLoss()
     opt = torch.optim.Adam(unet.parameters(), lr=learn_rate)
 
-    #do some training
-    train_loss, valid_loss = train(unet, train_data, valid_data, loss_fn, opt, acc_metric, epochs=epochs_val, params_path=params_path)
+    # do some training
+    train_loss, valid_loss = train(unet, train_data, valid_data, loss_fn, opt, acc_metric, epochs=epochs_val,
+                                   params_path=params_path)
 
-    #plot training and validation losses
+    # plot training and validation losses
     if visual_debug:
-        plt.figure(figsize=(10,8))
+        plt.figure(figsize=(10, 8))
         plt.plot(train_loss, label='Train loss')
         plt.plot(valid_loss, label='Valid loss')
         plt.legend()
         plt.show()
 
-    #predict on the next train batch (is this fair?)
+    # predict on the next train batch (is this fair?)
     xb, yb = next(iter(train_data))
     with torch.no_grad():
         predb = unet(xb.cuda())
 
-
-    #show the predicted segmentations
+    # show the predicted segmentations
     if visual_debug:
-        fig, ax = plt.subplots(bs,3, figsize=(15,bs*5))
+        fig, ax = plt.subplots(bs, 3, figsize=(15, bs * 5))
         for i in range(bs):
-            ax[i,0].imshow(batch_to_img(xb,i))
-            ax[i,1].imshow(yb[i])
-            ax[i,2].imshow(predb_to_mask(predb, i))
+            ax[i, 0].imshow(batch_to_img(xb, i))
+            ax[i, 1].imshow(yb[i])
+            ax[i, 2].imshow(predb_to_mask(predb, i))
 
         plt.show()
+
 
 if __name__ == "__main__":
     main()
