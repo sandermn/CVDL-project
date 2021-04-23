7d84ec0e77aa538b2e95f9ea3ea657692e98a30c
Olav Meberg <56315637+olavrm@users.noreply.github.com>
2021-04-23 14:09:12 +0200

    Update Unet2D.py

--- Unet2D.py
+++ Unet2D.py
@@ -1,28 +1,48 @@
 import torch
 from torch import nn
 
+
+
+
+
+
+
 class Unet2D(nn.Module):
+
+    #added one deeper lvl of conv layers
     def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        self.conv1 = self.contract_block(in_channels, 32, 7, 3)
-        self.conv2 = self.contract_block(32, 64, 3, 1)
-        self.conv3 = self.contract_block(64, 128, 3, 1)
+        filter = [32, 64, 128, 256, 512]
 
-        self.upconv3 = self.expand_block(128, 64, 3, 1)
-        self.upconv2 = self.expand_block(64*2, 32, 3, 1)
-        self.upconv1 = self.expand_block(32*2, out_channels, 3, 1)
+        self.conv1 = self.contract_block(in_channels, filter[0], 7, 3)
+        self.conv2 = self.contract_block(filter[0], filter[1], 3, 1)
+        self.conv3 = self.contract_block(filter[1], filter[2], 3, 1)
+        self.conv4 = self.contract_block(filter[2], filter[3], 3, 1)
+        self.conv5 = self.contract_block(filter[3], filter[4], 3, 1)
+
+        self.upconv5 = self.expand_block(filter[4], filter[3], 3, 1)
+        self.upconv4 = self.expand_block(filter[3], filter[2], 3, 1)
+        self.upconv3 = self.expand_block(filter[2], filter[1], 3, 1)
+        self.upconv2 = self.expand_block(filter[1], filter[0], 3, 1)
+        self.upconv1 = self.expand_block(filter[0], out_channels, 3, 1)
 
     def __call__(self, x):
         # downsampling part
+        #print(size(x))
         conv1 = self.conv1(x)
         conv2 = self.conv2(conv1)
         conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+        conv5 = self.conv5(conv4)
 
         #upsample
-        upconv3 = self.upconv3(conv3)
+        upconv5 = self.upconv5(conv4)
+        upconv4 = self.upconv4(torch.cat([upconv5, conv3]), 1)
+        upconv3 = self.upconv3(torch.cat([upconv4, conv3]), 1)
         upconv2 = self.upconv2(torch.cat([upconv3, conv2], 1))
         upconv1 = self.upconv1(torch.cat([upconv2, conv1], 1))
+
 
         return upconv1
 
@@ -35,7 +55,8 @@ class Unet2D(nn.Module):
 
             torch.nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding),
             torch.nn.BatchNorm2d(out_channels),
-            torch.nn.ReLU(),
+            #torch.nn.ReLU(),
+            torch.nn.LeakyReLU(),
 
             torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
                                  )
@@ -50,9 +71,10 @@ class Unet2D(nn.Module):
 
                             torch.nn.Conv2d(out_channels, out_channels, kernel_size, stride=1, padding=padding),
                             torch.nn.BatchNorm2d(out_channels),
-                            torch.nn.ReLU(),
+                            torch.nn.LeakyReLU(),
 
-                            torch.nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1) 
+                            #torch.nn.ReLU(),
+
+                            torch.nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
                             )
         return expand
-    
